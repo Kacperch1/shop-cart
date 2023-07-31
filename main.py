8b7847a20a128a41711ebdf0e705ade32463f331
Kacperch1 <kacper0602@interia.pl>
2023-07-31 09:25:49 +0200

    add-function-to-main-and-add-option-to-buy-specify-ammount-of-products

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
 from cart import Cart
 from product import Product
 
+##creating fuction which add products to cart
+def buy_products(cart, products):
+    while True:
+        product_name = input("What product do you need (type 'done' to finish shopping): ").lower()
+        if product_name == "done":
+            break
+
+        if product_name in products:
+            product = products[product_name]
+            quantity = int(input(f"How many {product_name}s do you want? "))
+            cart.products.extend([product] * quantity)
+            print(f"Added {quantity} {product_name}(s) to your cart.")
+        else:
+            print(f"Sorry, we don't have {product_name} in stock.")
+
+    return cart
+
 if __name__ == "__main__":
     cart = Cart()
     apple = Product("apple", 0.5)
     bread = Product("bread", 6)
     water = Product("water", 2)
     orange = Product("orange", 1)
-##starting talk with shop assistant
-    print(input("say hello: "))
-    print("good morning, we have apples, water, bread, and oranges. What do you need?")
-##adding products to cart
-    for i in range(3):
-        product_name = input(f"What product do you need ({i + 1}/3): ").lower()
-        if product_name == "apple":
-            cart.products.append(apple)
-            print("Added apple to your cart.")
-        elif product_name == "bread":
-            cart.products.append(bread)
-            print("Added bread to your cart.")
-        elif product_name == "water":
-            cart.products.append(water)
-            print("Added water to your cart.")
-        elif product_name == "orange":
-            cart.products.append(orange)
-            print("Added orange to your cart.")
-        else:
-            print(f"Sorry, we don't have {product_name} in stock.")
+
+    print(input("Say hello: "))
+    print("Good morning, we have apples, water, bread, and oranges. What do you need?")
+
+    products = {"apple": apple, "bread": bread, "water": water, "orange": orange}
+    cart = buy_products(cart, products)
 
     print("Here is your cart:")
     for product in cart.products:
